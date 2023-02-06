864a3f6849b31b0830e9bc582fdc774d7e7644bd
Bruno.S.Silva <85579710+BrunoSchmitz4@users.noreply.github.com>
2023-02-06 09:21:20 -0300

    Delete Imersão Dev directory

--- Imersão Dev/Aula 1 - Conversor de Temperatura/main.js
+++
@@ -1,7 +0,0 @@
-var celcius = 24;
-var fahrenheits = document.getElementById("main-f");
-var kelvins = document.getElementById("main-k");
-
-kelvin = celcius + 273.15
-fahrenheits.innerHTML = "Celcius: 24° -> Fahrenheit: "  + ((celcius * 1.8) + 32) + "°";
-kelvins.innerHTML = "Celcius: 24° -> Kelvin: "  +  kelvin + "°";
